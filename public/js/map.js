0c3763c531a35d1d8dc2461b7fe8e6b37d095850
oleg-pro <opr@meral.com.ua>
2018-05-14 11:17:59 +0300

    first commit

---
+++ public/js/map.js
@@ -0,0 +1,24 @@
+
+let devicesInMenu = {
+    'curentDeviceId': undefined,
+    'addCkickOnMenuEventListener' : function () {
+
+        let self = this; 
+
+        let lists = document.getElementsByClassName('devices-list');
+
+        let count_el = lists.length;
+
+        if (count_el > 0){
+            for(let i = 0; i < count_el; i ++){
+                lists[i].addEventListener('click',function(){
+                    for(let j = 0; j < count_el; j ++){
+                        lists[j].classList.remove('active');
+                    }
+                    this.classList.add('active');
+                    self.curentDeviceId = this.dataset.id;
+                });
+            }
+        }
+    }
+}
